c6f5878a7b7dfa6a6a8022ca8a68328244a4dc24
GregBerezhnov <greg.berezhnov@gmail.com>
2023-03-24 15:16:52 -0700

    server can add history

--- feeder_app/src/pages/FeederPage.vue
+++ feeder_app/src/pages/FeederPage.vue
@@ -1,28 +1,36 @@
 <template>
-<q-page padding>
+  <q-page padding>
     <div class="row q-col-gutter-md">
       <div class="col-6">
-        <q-card style="height: 555px; width: 500px;" class="row items-center justify-evenly">
+        <q-card
+          style="height: 555px; width: 500px"
+          class="row items-center justify-evenly"
+        >
           <q-card-section>
-          <q-scroll-area style="height: 500px; width: 400px;">
-            <feeding-feed />
-          </q-scroll-area>
-        </q-card-section>
+            <q-scroll-area style="height: 500px; width: 400px">
+              <feeding-feed :is-loading="isLoading" :events="events" />
+            </q-scroll-area>
+          </q-card-section>
         </q-card>
       </div>
       <div class="col-6">
         <div>
-          <q-img src="https://cdn.quasar.dev/img/parallax2.jpg"></q-img>
-          <!--<q-video
-            :ratio="16 / 9"
-             src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0"
-          />-->
+          <q-img
+            class="shadow-1"
+            style="border-radius: 6px"
+            src="https://cdn.quasar.dev/img/parallax2.jpg"
+          ></q-img>
         </div>
-        <div >
-          <q-toggle v-for="(_, i) in numInputs" v-model="toggled[i]" :key="i" keep-color/>
-          <q-btn @click="sendRequest">Apply</q-btn>
-          <q-btn @click="sendCatRequest">Cat</q-btn>
-          <q-btn @click="sendDogRequest">Dog</q-btn>
+        <div>
+          <q-toggle
+            v-for="(_, i) in numInputs"
+            v-model="toggled[i]"
+            :key="i"
+            keep-color
+          />
+          <q-btn>Apply</q-btn>
+          <q-btn @click="feedCat">Cat</q-btn>
+          <q-btn @click="feedDog">Dog</q-btn>
         </div>
       </div>
     </div>
@@ -31,58 +39,108 @@
 
 <script setup lang="ts">
 import FeedingFeed from "components/FeedingFeed.vue";
-import { reactive } from "vue";
+import { onMounted, reactive } from "vue";
 import { useRoute } from "vue-router";
+import axios from "axios";
+import { computedAsync, useAsyncState } from "@vueuse/core";
+import { ref } from "vue";
+import { z } from "zod";
+import { FeedEvent } from "components/models";
 
-const numInputs = 4
+const numInputs = 4;
 
-const toggled = reactive(Array.from({ length: numInputs }, () => false ))
+const toggled = reactive(Array.from({ length: numInputs }, () => false));
 
 const route = useRoute();
 
 const cat = {
-  request: "Left"
-};
+  request: "Left",
+} as const;
 
 const dog = {
-  request: "Right"
-};
+  request: "Right",
+} as const;
 
-
-async function sendRequest() {
-  const response = await fetch('http://localhost:5000/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(toggled)
-  });
-  const data = await response.json();
-  console.log(data);
+interface FeedSide {
+  request: "Left" | "Right";
 }
 
-async function sendCatRequest() {
-  const response = await fetch('http://localhost:5000/api/v1/users/create', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(cat)
-  });
-  const data = await response.json();
-  console.log(data);
+const feedResponse = z.object({ request: z.string() });
+
+async function sendFeedRequest(data: { request: "Left" | "Right" }) {
+  const response = await axios.post("http://localhost:5000/api/feed", data);
+
+  console.log(response.data);
+
+  const responseData = feedResponse.safeParse(response.data);
+
+  if (!responseData.success) {
+    console.error("Invalid response", response.data);
+    return;
+  }
+
+  // app.post('/feed/:id', (req, res) => {
+  //   const body = feedRequestBody.safeParse(req.body);
+  //   if(!body.success)
+  //     return res.sendStatus(400);
+
+  //   const id = req.params.id;
+
+  //   const pet = await db.pets.findById(id);
+
+  //   if(!pet)
+  //     return res.sendStatus(404);
+
+  //   // feed pet logic
+
+  //   const entry = await db.insert({
+  //     petId: id,
+  //     title: 'Fed ' + pet.name,
+  //     date: new Date()
+  //   })
+
+  //   return res.json(entry);
+  // })
+
+  if (responseData.data.request === "dog-success") {
+    events.value.unshift({
+      id: 1,
+      title: "Fed Dog",
+      date: new Date().toLocaleTimeString("en-US"),
+    });
+    return;
+  }
+  if (responseData.data.request === "cat-success") {
+    events.value.unshift({
+      id: 2,
+      title: "Fed Cat",
+      date: new Date().toLocaleTimeString("en-US"),
+    });
+    return;
+  } else {
+    console.error("Invalid response", response.data);
+    return;
+  }
 }
 
-async function sendDogRequest() {
-  const response = await fetch('http://localhost:5000/api/v1/users/create', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(dog)
-  });
-  const data = await response.json();
-  console.log(data);
+async function feedCat() {
+  sendFeedRequest(cat);
 }
 
+async function feedDog() {
+  sendFeedRequest(dog);
+}
+
+const getEvents = () =>
+  new Promise<FeedEvent[]>((resolve) =>
+    setTimeout(() => resolve([] as FeedEvent[]), 500)
+  );
+
+const events = ref([] as FeedEvent[]);
+const isLoading = ref(false);
+onMounted(async () => {
+  isLoading.value = true;
+  events.value = await getEvents();
+  isLoading.value = false;
+});
 </script>
