781dc51d303251d53dc9e87949691dc512ebc42c
navrajb <navrajb03@gmail.com>
2023-03-29 08:47:14 -0700

    updated camera

--- image_sharing/image_reciever.py
+++ image_sharing/image_reciever.py
@@ -1,21 +1,21 @@
 import socket
 import cv2
+import time
 
 # create socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# get local machine name
-host = socket.gethostname()
-port = 12345
-
-# bind socket to a public host, and a port
+# bind socket to a specific IP address and port
+host = '206.87.97.60' # replace with your server's IP address
+port = 5000
 s.bind((host, port))
 
 # set the maximum number of queued connections
 s.listen(5)
 
 while True:
-    # establish a connection
+    # wait for a client to connect
+    print('Waiting for client to connect...')
     client_socket, addr = s.accept()
     print('Got connection from', addr)
 
@@ -27,9 +27,14 @@ while True:
     encoded, buffer = cv2.imencode('.jpg', frame)
     data = buffer.tobytes()
 
-    # send the image to the client
-    client_socket.sendall(data)
+    # send the image size and data to the client
+    size = len(data)
+    header = f'{size:012d}'
+    client_socket.sendall(header.encode() + data)
 
     # release the resources
     cap.release()
     client_socket.close()
+
+    # wait for 5 seconds before capturing the next image
+    time.sleep(5)
